49501abda8aec6e0c8e092a303613310b5842d09
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2023-04-18 23:21:09 +0530

    removed extra slash from base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://timetablle-api-automation.vercel.app//api/v1",
+  baseURL: "https://timetablle-api-automation.vercel.app/api/v1",
 });
